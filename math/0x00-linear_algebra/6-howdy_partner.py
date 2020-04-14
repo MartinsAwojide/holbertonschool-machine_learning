6e01a85e96598ea6554bdb1bf921f0b0130de63b
jucortesa <984@holbertonschool.com>
2020-04-14 12:17:00 -0500

    [feature] concatenate two arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def cat_arrays(arr1, arr2):
+    """Function that concatenates two arrays
+
+    Arguments:
+        arr1 {[list]} --  are lists of ints/floats
+        arr2 {[list]} --  are lists of ints/floats
+
+    Returns:
+        [list] -- New list with
+    """
+    return arr1 + arr2
