e395e2bc32ec4d399718ceab178cc5bf4bbc1607
jucortesa <984@holbertonschool.com>
2020-04-14 12:19:22 -0500

    [feature] shape ndarray

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def np_shape(matrix):
+    """A funtion that calculates the shape of a numpy.ndarray
+
+    Arguments:
+        matrix {[ndarray]}
+
+    Returns:
+        [ndarray]
+    """
+    return matrix.shape
