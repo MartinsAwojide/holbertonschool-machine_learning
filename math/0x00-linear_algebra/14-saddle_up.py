4efbd9c5fe0dbc91cf984b00405d4000eae5519e
jucortesa <984@holbertonschool.com>
2020-04-14 12:21:50 -0500

    [feature] matrix multiplication ndarray

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function that performs matrix multiplication
+
+    Arguments:
+        mat1 {[ndarray]}
+        mat2 {[ndarray]}
+
+    Returns:
+        [ndarray]
+    """
+    return np.matmul(mat1, mat2)
