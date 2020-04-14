15bd8041a8430cd7d0ac6d10916a2ac390c3dcf0
jucortesa <984@holbertonschool.com>
2020-04-14 12:20:00 -0500

    [feature] transposes ndarray

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def np_transpose(matrix):
+    """A function that transposes matrix
+
+    Arguments:
+        matrix {[ndarray]}
+
+    Returns:
+        [ndarray]x
+    """
+    return matrix.T
