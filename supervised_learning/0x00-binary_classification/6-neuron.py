661ffe1d81729bf04f5ad2ed4030c4f18e0a0929
jucortesa <984@holbertonschool.com>
2020-05-05 21:30:59 -0500

    [fix] change evaluate

--- supervised_learning/0x00-binary_classification/6-neuron.py
+++ supervised_learning/0x00-binary_classification/6-neuron.py
@@ -74,8 +74,8 @@ class Neuron:
         """
         self.forward_prop(X)
         cost = self.cost(Y, self.__A)
-        self.__A = np.where(self.__A >= 0.5, 1, 0)
-        return self.__A, cost
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        return prediction, cost
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
         """
@@ -114,7 +114,7 @@ class Neuron:
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
-            raise ValueError("iterations must be a positive integer")
+            raise ValueError("1iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
