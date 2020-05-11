3385fa354fc7a01fe8cb38c3bbb88da63cc082d3
jucortesa <984@holbertonschool.com>
2020-05-11 18:29:00 -0500

    [feature] one hot encode

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    converts a numeric label vector into a one-hot matrix
+    :param Y: is a ndarray with shape (m,) containing numeric class labels
+              m is the number of examples
+    :param classes:  is the maximum number of classes found in Y
+    :return:
+    """
+    if type(classes) is not int or classes <= max(Y):
+        return None
+    if type(Y) is not np.ndarray or len(Y) == 0:
+        return None
+    encode = (np.identity(classes)[Y])  # use the identity array where is Y
+    # np identity is a special case of np eye
+    return encode.T
