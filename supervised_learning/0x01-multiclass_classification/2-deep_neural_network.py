2166dc65b0e6021f02a9bf03e16440f089c5049b
jucortesa <984@holbertonschool.com>
2020-05-12 17:44:09 -0500

    [fix] if condition in previous task

--- supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
@@ -163,7 +163,7 @@ class DeepNeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        if verbose is True and graph is True:
+        if verbose is True or graph is True:
             if type(step) is not int:
                 raise TypeError("step must be an integer")
             if step <= 0 or step > iterations:
