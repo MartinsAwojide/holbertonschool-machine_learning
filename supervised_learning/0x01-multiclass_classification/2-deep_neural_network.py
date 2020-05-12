ed711465a8e5f7e2cd5c1bc6392781e213bb4a1c
jucortesa <984@holbertonschool.com>
2020-05-12 17:06:41 -0500

    [fix] if condition in previous task

--- supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
@@ -155,6 +155,21 @@ class DeepNeuralNetwork:
         :return: the evaluation of the training data after
         iterations of training have occurred
         """
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        if verbose is True and graph is True:
+            if type(step) is not int:
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iterations:
+                raise ValueError("step must be positive and <= iterations")
+        if type(step) is not int:
+            raise TypeError("step must be an integer")
         graph_iteration = []
         graph_cost = []
 
