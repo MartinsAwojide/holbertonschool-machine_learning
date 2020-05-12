8e73c64171f8af8c37dff7d7d67362546a1b7a94
jucortesa <984@holbertonschool.com>
2020-05-12 10:59:54 -0500

    [feature] condition matrix shape and length

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -14,5 +14,7 @@ def one_hot_decode(one_hot):
     """
     if type(one_hot) is not np.ndarray:
         return None
+    if len(one_hot.shape) != 2 or len(one_hot) == 0:
+        return None
 
     return np.argmax(one_hot, axis=0)
