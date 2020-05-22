606ebb2c2b744e1ea65100a6401a211d0b56a3f7
jucortesa <984@holbertonschool.com>
2020-05-21 20:09:00 -0500

    [feature] shuffle data

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Shuffle data"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+    shuffles the data points in two matrices the same way
+    :param X:  is the first numpy.ndarray of shape (m, nx) to shuffle
+            m is the number of data points
+            nx is the number of features in X
+    :param Y: is the second numpy.ndarray of shape (m, ny) to shuffle
+            m is the same number of data points as in X
+            ny is the number of features in Y
+
+    :return: the shuffled X and Y matrices
+    """
+    m = X.shape[0]
+    permutation = list(np.random.permutation(m))
+    print(permutation)
+    shuffled_X = X[permutation]
+    shuffled_Y = Y[permutation]
+    return shuffled_X, shuffled_Y
