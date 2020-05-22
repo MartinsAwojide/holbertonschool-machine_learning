dc6b1fb55daad0cd7dae9df27425dbab9df617d2
jucortesa <984@holbertonschool.com>
2020-05-22 11:27:51 -0500

    [feature] RMSProp optimization

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""RMSprop train"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    creates the training operation for a neural network in tensorflow using
+    the RMSProp optimization algorithm
+    :param loss: is the loss of the network
+    :param alpha: is the learning rate
+    :param beta2: is the RMSProp weight
+    :param epsilon: is a small number to avoid division by zero
+    :return: the RMSProp optimization operation
+    """
+    return tf.train.RMSPropOptimizer(learning_rate=alpha, momentum=beta2,
+                                     epsilon=epsilon).minimize(loss)
