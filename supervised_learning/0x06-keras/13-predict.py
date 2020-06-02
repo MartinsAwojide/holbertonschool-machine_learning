8b50a37132a20b6bf32612ab5c180c1bc4f4b2dc
jucortesa <984@holbertonschool.com>
2020-06-02 14:13:18 -0500

    [feature] predict

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""prediction"""
+
+
+def predict(network, data, verbose=False):
+    """
+    makes a prediction using a neural network
+    :param network: is the network model to test
+    :param data: is the input data to test the model with
+    :param verbose: is a boolean that determines if output should be
+    printed during the testing process
+    :return: the prediction for the data
+    """
+    return network.predict(x=data, verbose=verbose)
