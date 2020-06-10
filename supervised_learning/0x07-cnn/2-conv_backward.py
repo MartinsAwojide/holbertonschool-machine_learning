[fix] change formula

supervised_learning/0x07-cnn/2-conv_backward.py
@@ -34,7 +34,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     Returns: the partial derivatives with respect to the previous
     layer (dA_prev), the kernels (dW), and the biases (db), respectively
     """
-    m, h_prev, w_prev, c_prev = A_prev.shape
+    m, h_prev, w_prev, _ = A_prev.shape
     _, h_new, w_new, c_new = dZ.shape
     kh, kw, c_prev, _ = W.shape
     sh, sw = stride
@@ -46,7 +46,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     # initialize the derivatives
     dA_prev = np.zeros(A_prev.shape)
     dW = np.zeros(W.shape)
-    db = np.zeros(b.shape)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     # padding
     A_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w),