2f176304658a5b28e311229a60e9a12434bf3842
jucortesa <984@holbertonschool.com>
2020-06-15 14:59:09 -0500

    [fix] change to commit 3a3c6c6979a687454e53ba076ba5313627fab853

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
-import sys
+"""train model"""
+import tensorflow.keras as K
 
-input = {'200': 0,
-         '301': 0,
-         '400': 0,
-         '401': 0,
-         '403': 0,
-         '404': 0,
-         '405': 0,
-         '500': 0}
-status_code = ""
-file_size = ""
-counter = 0
-for line in sys.stdin:
-    aux = line.split()
-    status_code = aux[-2]
-    file_size += int(aux[-1])
-    if status_code in input:
-        input[status_code] += 1
-    if counter % 10 == 0:
-        print(file_size)
-    counter += 1
-    print(input)
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    Train the model using early stopping
+    :param network: is the model to train
+    :param data: ndarray of shape (m, nx) containing the input data
+    :param labels: is a one-hot numpy.ndarray of shape (m, classes)
+    containing the labels of data
+    :param batch_size: size of the batch used for mini-batch gradient descent
+    :param epochs:number of passes through data for mini-batch gradient descent
+    :param validation_data: is the data to validate the model with, if not None
+    :param early_stopping: indicates whether early stopping should be used
+    early stopping should only be performed if validation_data exists
+    early stopping should be based on validation loss
+    :param patience: is the patience used for early stopping
+    :param verbose: boolean that determines if output should be printed
+    :param shuffle: boolean that determines whether to shuffle the batches
+    every epoch. Normally, it is a good idea to shuffle, but for
+    reproducibility, we have chosen to set the default to False.
+    :return:
+    """
+    early = []
+    if early_stopping and validation_data:
+        early.append(K.callbacks.EarlyStopping(patience=patience))
+    history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
+                          shuffle=shuffle, verbose=verbose, callbacks=early,
+                          validation_data=validation_data)
+    return history
