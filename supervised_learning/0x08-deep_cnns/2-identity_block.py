a5f1103789ee33f5a12565ac11fea0c4e2109922
jucortesa <984@holbertonschool.com>
2020-06-16 12:03:33 -0500

    [feature] identity block

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Identity block"""
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """
+    Builds an identity block for ResNet
+    Args:
+        A_prev: is the output from the previous layer
+        filters: is a tuple or list containing F11, F3, F12, respectively:
+                - F11 is the number of filters in the first 1x1 convolution
+                - F3 is the number of filters in the 3x3 convolution
+                - F12 is the number of filters in the second 1x1 convolution
+    Returns: the activated output of the identity block
+    """
+
+    init = K.initializers.he_normal()
+    F11, F3, F12 = filters
+    X = K.layers.Conv2D(filters=F11, kernel_size=1, kernel_initializer=init,
+                        padding='same', strides=(1, 1))(A_prev)
+    #  along the channels axis. A_prev.shape=(None, 224, 224, 256)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(filters=F3, kernel_size=3, kernel_initializer=init,
+                        padding='same', strides=(1, 1))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(filters=F12, kernel_size=1, kernel_initializer=init,
+                        padding='same', strides=(1, 1))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Add()([X, A_prev])
+    X = K.layers.Activation('relu')(X)
+
+    return X
