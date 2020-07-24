187dd67b964d80b637f89c9315cc4ca77219e06d
jucortesa <984@holbertonschool.com>
2020-07-24 09:02:37 -0500

    [feature] add condition if matrix is symmetric

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -4,13 +4,29 @@ import numpy as np
 
 
 def definiteness(matrix):
+    """
+    Calculates the definiteness of a matrix
+    Args:
+        matrix: is a numpy.ndarray of shape (n, n) whose definiteness
+        should be calculated
+
+    Returns:the string Positive definite, Positive semi-definite,
+    Negative semi-definite, Negative definite, or Indefinite
+    If matrix does not fit any of the above categories, return None
+    """
 
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
+
     if len(matrix) < 2:
         return None
     row, col = matrix.shape
+
     if row != col:
+        return None
+
+    # Matrix has to be symmetric to calculate definiteness
+    if not (matrix == matrix.T).all():
         return None
 
     eigen = np.linalg.eigvals(matrix)
