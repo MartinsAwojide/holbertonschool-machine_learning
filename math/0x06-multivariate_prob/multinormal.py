60682cfd435b27d0b2a61b6bf26ad8df5133c58c
jucortesa <984@holbertonschool.com>
2020-07-27 15:25:37 -0500

    [feature] add pdf

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -22,3 +22,27 @@ class MultiNormal():
         self.mean = np.mean(data, axis=1).reshape(d, 1)
         num = data - self.mean
         self.cov = np.dot(num, num.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point
+        Args:
+            x: is a numpy.ndarray of shape (d, 1) containing
+            the data point whose PDF should be calculated
+
+        Returns: the value of the P
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        part1 = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+        part2 = np.dot((x-self.mean).T, inv)
+        part3 = np.dot(part2, (x - self.mean) / -2)
+        pdf = part1 * np.exp(part3)
+
+        return pdf[0][0]
