c5b289aa2bd2545b5d048a580aa583d1ed04e57d
jucortesa <984@holbertonschool.com>
2020-07-28 21:57:42 -0500

    [feature] Initialize t-SNE

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Initializes all variables required to calculate the P affinities in t-SNE
+"""
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """
+    Initializes all variables required to calculate the P affinities in t-SNE
+    Args:
+        X: is a numpy.ndarray of shape (n, d) containing the dataset
+        to be transformed by t-SNE
+        perplexity: is the perplexity that all Gaussian distributions
+        should have
+
+    Returns: (D, P, betas, H)
+    D: a numpy.ndarray of shape (n, n) that calculates
+    the pairwise distance between two data points
+    P: a numpy.ndarray of shape (n, n) initialized to all 0‘s that
+    will contain the P affinities
+    betas: a numpy.ndarray of shape (n, 1) initialized to all 1’s
+    that will contain all of the beta values
+    H: is the Shannon entropy for perplexity perplexity
+    """
+    (n, d) = X.shape
+    # np.square is the elementwise square; axis=1 means sum the rows.
+    # so sum_X is the vector of the norms of the x_i
+    sum_X = np.sum(np.square(X), 1)
+    # ||x_i-x_j||^2=||x_i||^2+||x_j||**2-2(x_i,x_j)
+    # np.dot(X,X.T) has entries (x_i,x_j)
+    # in position (i,j) you add ||x_i||^2 and ||x_j||^2 from the two sums
+    # with the transpose it is added the x_j^2 missing for each ||x_i-x_j||^2
+    # the result is that D is a symmetric matrix with the pairwise distances.
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    betas = np.ones((n, 1))
+    P = np.zeros((n, n))
+    H = np.log2(perplexity)
+
+    return D, P, betas, H
