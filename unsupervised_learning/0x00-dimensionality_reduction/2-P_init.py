6c25d1a3b1fbb2a88702315e3f310b4e8f79d624
jucortesa <984@holbertonschool.com>
2020-07-29 17:39:32 -0500

    [fix] back to previous with diagonal with zeros

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -23,6 +23,17 @@ def P_init(X, perplexity):
     that will contain all of the beta values
     H: is the Shannon entropy for perplexity perplexity
     """
+    (n, d) = X.shape
+    # np.square is the elementwise square; axis=1 means sum the rows.
+    # so sum_X is the vector of the norms of the x_i
+    sum_X = np.sum(np.square(X), 1)
+    # ||x_i-x_j||^2=||x_i||^2+||x_j||**2-2(x_i,x_j)
+    # np.dot(X,X.T) has entries (x_i,x_j)
+    # in position (i,j) you add ||x_i||^2 and ||x_j||^2 from the two sums
+    # with the transpose it is added the x_j^2 missing for each ||x_i-x_j||^2
+    # the result is that D is a symmetric matrix with the pairwise distances.
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    """ another version
     n, _ = X.shape
     # new axis to broadcast (1, 2500, 50) - (2500, 1, 50)
     # by broadcasting it operates each element
@@ -31,6 +42,9 @@ def P_init(X, perplexity):
     X2 = X[:, np.newaxis, :]
     # if operates in axis 2, it desapears and becomes (2500, 2500)
     D = np.sum(np.square(X1 - X2), axis=2)
+    """
+
+    np.fill_diagonal(D, 0)
     betas = np.ones((n, 1))
     P = np.zeros((n, n))
     H = np.log2(perplexity)
