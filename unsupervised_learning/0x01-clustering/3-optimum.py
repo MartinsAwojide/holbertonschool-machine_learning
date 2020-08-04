f6fd28aa1987b2b65a0532914ea70dce10e62207
jucortesa <984@holbertonschool.com>
2020-08-04 15:53:20 -0500

    [feature] optimum k

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Tests for the optimum number of clusters by variance"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Tests for the optimum number of clusters by variance
+    Args:
+        X: is a numpy.ndarray of shape (n, d) containing the data set
+        kmin: is a positive integer containing the minimum number of
+        clusters to check for (inclusive)
+        kmax: is a positive integer containing the maximum number of
+        clusters to check for (inclusive)
+        iterations: is a positive integer containing the maximum number
+        of iterations for K-means
+
+    Returns: results, d_vars, or None, None on failure
+    - results is a list containing the outputs of K-means for each cluster size
+    - d_vars is a list containing the difference in variance from the
+    smallest cluster size for each cluster size
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if kmax is not None and (type(kmax) is not int or kmax < 1):
+        return None, None
+    if kmax is not None and kmin >= kmax:
+        return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+
+    n, _ = X.shape
+    if kmax is None:
+        kmax = n
+
+    results = []
+    d_vars = []
+    for i in range(kmin, kmax + 1):
+        centroids, clss = kmeans(X, i, iterations)
+        results.append((centroids, clss))
+
+        if i == kmin:
+            var_initial = variance(X, centroids)
+        var = variance(X, centroids)
+        d_vars.append(var_initial - var)
+
+    return results, d_vars
