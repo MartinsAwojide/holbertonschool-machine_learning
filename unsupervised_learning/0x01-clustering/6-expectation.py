f12239c06415e99639fe23b63ed53078ea1265f3
jucortesa <984@holbertonschool.com>
2020-08-05 16:42:24 -0500

    [fix] type condition

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -21,19 +21,17 @@ def expectation(X, pi, m, S):
     - likehood is the total log likelihood
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(m) is not np.ndarray or len(m.shape) != 1:
-        return None
-    if type(S) is not np.ndarray or len(S.shape) != 2:
-        return None
-    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
-        return None
-    if S.shape[0] != S.shape[1]:
-        return None
+        return None, None
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
+        return None, None
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
+
     n, d = X.shape
     k = pi.shape[0]
-    den = 0
-    g = np.zeros((k, n))
+
     if k > n:
         return None, None
     if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
@@ -42,7 +40,8 @@ def expectation(X, pi, m, S):
         return None, None
     if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
-
+    den = 0
+    g = np.zeros((k, n))
     for i in range(k):
         # πk * N(xi∣μk,Σk)
         aux = pi[i] * pdf(X, m[i], S[i])
