7e4662876426846174322d358ff91ad2793a0c55
jucortesa <984@holbertonschool.com>
2020-08-05 20:23:19 -0500

    [feature] pycodestyle

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -13,7 +13,7 @@ def gmm(X, k):
     Returns: pi, m, S, clss, bic
     - pi is a numpy.ndarray of shape (k,) containing the cluster priors
     - m is a numpy.ndarray of shape (k, d) containing the centroid means
-    - S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
+    - S is a ndarray of shape (k, d, d) containing the covariance matrices
     - clss is a numpy.ndarray of shape (n,) containing the cluster indices
     for each data point
     - bic is a numpy.ndarray of shape (kmax - kmin + 1) containing
