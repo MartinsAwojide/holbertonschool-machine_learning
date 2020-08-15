87bb922a5bdbdfc3cd829aef5e52400407de9055
jucortesa <984@holbertonschool.com>
2020-08-14 20:57:20 -0500

    [fix] Conditions probs=1

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -68,21 +68,22 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
 
     Returns: the converged Transition, Emission, or None, None on failure
     """
-    if type(Observations) is not np.ndarray or len(Observations.shape) != 1:
+    if type(Observations) is not np.ndarray or len(Observation.shape) != 1:
         return None, None
     if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
         return None, None
     if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
         return None, None
-    if Transition.shape[0] != Transition.shape[1]:
-        return None, None
     if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
         return None, None
     if type(iterations) is not int or iterations <= 0:
         return None, None
-    if Initial.shape[0] != Transition.shape[0]:
+
+    if Transition.shape[0] != Initial.shape[0]:
         return None, None
-    if Emission.shape[0] != Transition.shape[0]:
+    if Transition.shape[0] != Emission.shape[0]:
+        return None, None
+    if Transition.shape[0] != Transition.shape[1]:
         return None, None
 
     N, M = Emission.shape
