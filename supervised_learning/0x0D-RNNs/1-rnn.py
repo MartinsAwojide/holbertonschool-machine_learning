aa2feb23248a61c1fd4027e4909f264976beb7f4
jucortesa <984@holbertonschool.com>
2020-09-02 12:00:11 -0500

    [feature] forward propagation for a simple RNN

---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""RNN forward"""
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+    Performs forward propagation for a simple RNN
+    Args:
+        X: is the data to be used, given as a numpy.ndarray of shape
+        (t, m, i)
+        - t is the maximum number of time steps
+        - m is the batch size
+        - i is the dimensionality of the data
+        h_0:  is the initial hidden state, given as a numpy.ndarray
+        of shape (m, h)
+        - h is the dimensionality of the hidden state
+    Returns: H, Y
+    - H is a numpy.ndarray containing all of the hidden states
+    - Y is a numpy.ndarray containing all of the outputs
+    """
+    t, m, i = X.shape
+    _, h = h_0.shape
+    h_prev = h_0
+    H = np.zeros((t+1, m, h))
+    Y = np.zeros((t, m, rnn_cell.Wy.shape[-1]))
+    H[0] = h_0
+    for i in range(t):
+        x_t = X[i]
+        h_prev, y_prev = rnn_cell.forward(h_prev, x_t)
+        H[i+1] = h_prev
+        Y[i] = y_prev
+    return H, Y
