259682647d24a1451bc168c340e7f6439fc44583
jucortesa <984@holbertonschool.com>
2020-10-06 09:55:34 -0500

    [feature] Pipeline

--- supervised_learning/0x12-transformer_apps/3-dataset.py
+++ supervised_learning/0x12-transformer_apps/3-dataset.py
@@ -8,19 +8,44 @@ tf.compat.v1.enable_eager_execution()
 
 class Dataset:
     """loads and preps a dataset for machine translation"""
-    def __init__(self):
+    def __init__(self, batch_size, max_len):
         """Class initializer"""
+        def filter_max_length(x, y, max_length=max_len):
+            """
+            filter method
+            """
+            return tf.logical_and(tf.size(x) <= max_length,
+                                  tf.size(y) <= max_length)
+
         examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                        with_info=True,
                                        as_supervised=True)
-        self.data_train, self.data_valid = examples['train'], \
-            examples['validation']
 
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
 
+        PT, EN = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = PT, EN
+
+        # ******** TRAIN DATA **********************
         self.data_train = self.data_train.map(self.tf_encode)
+        self.data_train = self.data_train.filter(filter_max_length)
+        # cache the dataset to memory to get a speedup while reading from it.
+        self.data_train = self.data_train.cache()
+
+        shu = metadata.splits['train'].num_examples
+        self.data_train = self.data_train.shuffle(shu)
+        pad_shape = ([None], [None])
+        self.data_train = self.data_train.padded_batch(batch_size,
+                                                       padded_shapes=pad_shape)
+        aux = tf.data.experimental.AUTOTUNE
+        self.data_train = self.data_train.prefetch(aux)
+
+        # ************ VALID DATA ********************
         self.data_valid = self.data_valid.map(self.tf_encode)
+        self.data_valid = self.data_valid.filter(filter_max_length)
+        self.data_valid = self.data_valid.padded_batch(batch_size,
+                                                       padded_shapes=pad_shape)
 
     def tokenize_dataset(self, data):
         """
